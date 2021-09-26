49512314877421542bc942afa110c49d496261fd
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-09-26 09:02:42 +0530

    Add files via upload

---
+++ ArrForeachLoopex3.java
@@ -0,0 +1,12 @@
+
+public class ArrForeachLoopex3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a[]= {10,20,30,90,120};
+for(int i:a) {//printing elements one by one using for each loop
+	System.out.println(i);
+}
+	}
+
+}
