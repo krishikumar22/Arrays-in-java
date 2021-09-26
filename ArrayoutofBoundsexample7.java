cd10c818ed084221e17a0486fe39512118eed579
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-09-26 09:03:28 +0530

    Add files via upload

---
+++ ArrayoutofBoundsexample7.java
@@ -0,0 +1,11 @@
+
+public class ArrayoutofBoundsexample7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a[]= {10,20,30,90,120};
+for(int i=0;i<=a.length;i++)
+	System.out.println(a[i]);
+	}
+
+}
