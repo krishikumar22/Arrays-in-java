514a2c4cd8dd34d5bb5fe0f48f8e381edcc85658
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-09-26 09:14:45 +0530

    Add files via upload

---
+++ Mularrayex8.java
@@ -0,0 +1,14 @@
+
+public class Mularrayex8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a[][]= {{1,2,3},{4,5,6},{6,7,8}};
+for(int i=0;i<3;i++) {
+	for(int j=0;j<3;j++) {
+		System.out.println(a[i][j]+" ");
+	}
+	System.out.println();
+}
+
+}}
