754a2690e2b0dade62fc426fa8b355e11e6276d0
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-09-26 10:09:02 +0530

    Add files via upload

---
+++ ClassNameArrayex10.java
@@ -0,0 +1,12 @@
+
+public class ClassNameArrayex10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a[]= {3,5,9};
+Class c=a.getClass();
+String name=c.getName();
+System.out.println(name);
+}
+
+}
