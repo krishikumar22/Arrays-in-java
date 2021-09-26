e13ccd4c904369c69b9faff2ee0cf5f8e0a46b72
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-09-26 09:02:12 +0530

    Add files via upload

---
+++ Arrayex1.java
@@ -0,0 +1,20 @@
+
+public class Arrayex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int[] a=new int[5];//declaration and instantation
+a[0]=10;
+a[1]=30;
+a[2]=48;//initialising array
+a[3]=12;
+a[4]=23;
+Object length;
+
+//traversing
+for(int i=0;i<a.length;i++) {
+	System.out.println(a[i]);
+}
+	}
+
+}
