dfce1fc825fa514f3959034af5ee2fe14878c9f3
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-09-26 09:02:26 +0530

    Add files via upload

---
+++ Arrayex2.java
@@ -0,0 +1,12 @@
+
+public class Arrayex2 {//printing java arrays in a single line
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a[]= {10,20,30,90,120};
+for(int i=0;i<a.length;i++) {
+	System.out.println(a[i]);
+}
+	}
+
+}
